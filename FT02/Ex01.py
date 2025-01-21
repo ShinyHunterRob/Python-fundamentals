d8156938541a438124f27ed2c5866a8c1764bb1a
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 10:18:36 +0000

    Solved Ex01

---
+++ FT02/Ex01.py
@@ -0,0 +1,10 @@
+print ("Vamos pedir-lhe 3 números, e devolver-lhe a soma dos mesmos.")
+num1 = int(input("Insira o primeiro número:\t "))
+num2 = int(input("Insira o segundo número:\t "))
+num3 = int(input("Insira o terceiro número:\t "))
+
+soma = num1 + num2 + num3
+
+print (f"{num1} + {num2} + {num3} = {soma}")
+
+print (f"A soma dos números inseridos é {soma}.")
