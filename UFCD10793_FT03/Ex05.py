6371d37221910374da3e2bdf2478c49060968e2f
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 12:42:35 +0000

    Solved ex05

---
+++ UFCD10793_FT03/Ex05.py
@@ -0,0 +1,8 @@
+num1 = int(input("Insira um número inteiro:\t"))
+
+if num1 == 0:
+    print(f"O número inserido é nulo (Zero).")
+elif num1 > 0:
+    print(f"O número inserido é positivo.")
+else:
+    print(f"O número inserido é negativo.")
