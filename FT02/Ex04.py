ff894efb25bc873da4e7695030c61841c2ea9bfa
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 10:28:34 +0000

    Solved Ex04

---
+++ FT02/Ex04.py
@@ -0,0 +1,7 @@
+print ("Vamos pedir-lhe uma temperatura em Fahrenheit, e convertê-la para Celsius.")
+temp_f = float(input("Insira a temperatura em Fahrenheit:\t "))
+
+
+temp_c = 5*((temp_f-32)/9)
+
+print (f"{temp_f}ºF em Celcius é {temp_c}ºC.")
