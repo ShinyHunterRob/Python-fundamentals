ce6f6feb064a28ffa23595fbd9116559c21c0c4f
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 12:31:09 +0000

    Solved Ex02

---
+++ UFCD10793_FT03/Ex02.py
@@ -0,0 +1,7 @@
+num1 = int(input("Insira um número inteiro:\t"))
+num2 = int(input("Insira outro número inteiro:\t"))
+
+if num1 == num2:
+    print(f"Os números inseridos são iguais")
+else:
+    print(f"Os números inseridos são diferentes")
