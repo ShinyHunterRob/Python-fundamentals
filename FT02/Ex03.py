ff83e5688f54ff6f60bfbc25ed0dc71744ae96d5
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 10:25:38 +0000

    Solved Ex03

---
+++ FT02/Ex03.py
@@ -0,0 +1,7 @@
+print ("Vamos pedir-lhe uma distância em kms, e convertê-la para metros.")
+metros = float(input("Insira a distância:\t "))
+
+
+kms = metros / 1000
+
+print (f"{metros} metros em kms é {kms}")
