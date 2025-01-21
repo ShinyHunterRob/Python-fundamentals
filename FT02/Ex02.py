c5f36f30be2afb494b1666a208e1c6ce6762cb5b
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-21 10:22:53 +0000

    Solved Ex02

---
+++ FT02/Ex02.py
@@ -0,0 +1,9 @@
+print ("Vamos pedir-lhe 2 notas, e calcular a média das mesmas.")
+num1 = int(input("Insira o primeiro número:\t "))
+num2 = int(input("Insira o segundo número:\t "))
+
+media = (num1 + num2) / 2
+
+print (f"({num1} + {num2})/2 = {media}")
+
+print (f"A média dos números inseridos é {media}.")
