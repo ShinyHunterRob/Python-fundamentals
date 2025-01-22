586b5952e1eee6c01d6c2968ccd501e9f6685543
ShinyHunterRob <51171149+ShinyHunterRob@users.noreply.github.com>
2025-01-22 10:30:03 +0000

    Fixed Ex06

--- UFCD10793_FT03/Ex06.py
+++ UFCD10793_FT03/Ex06.py
@@ -17,3 +17,5 @@ if num1 > num2 and num1 > num3:
     print(f"{num1} é o maior número.")
 elif num2 > num1 and num2 > num3:
     print(f"{num2} é o maior número.")
+else:
+    print(f"{num3} é o maior número.")
